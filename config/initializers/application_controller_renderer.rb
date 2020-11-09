c0e8ba66696027af755354272a63f31f417c7a9c
EC2 Default User <ec2-user@ip-172-31-23-201.ec2.internal>
2020-11-09 13:46:27 +0000

    [add]コメント追加機能まで

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1025483b39e3b63b9b56bbb4ee15fff69d526d03183207660fecb950afd1064fa7e9582b79d9f22a7222762caa3653fe7b7a34cb29719d58eb508883eb042005'
